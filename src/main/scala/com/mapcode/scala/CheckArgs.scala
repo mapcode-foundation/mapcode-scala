4fa7c8374c01c875d0c41ed9051598f634149137
Eric Bowman <eric.bowman@zalando.de>
2014-10-24 09:56:25 +0200

    CheckArgs.checkRange becomes generic

--- src/main/scala/com/mapcode/scala/CheckArgs.scala
+++ src/main/scala/com/mapcode/scala/CheckArgs.scala
@@ -16,8 +16,9 @@
 package com.mapcode.scala
 
 private[scala] object CheckArgs {
-  private[mapcode] def checkRange(param: String, value: Double, min: Double, max: Double): Unit =
+  private[mapcode] def checkRange[T <% Ordered[T]](param: =>String, value: T, min: T, max: T): Unit = {
     require(value >= min && value <= max, s"Parameter $param should be in range [$min, $max], but is: $value")
+  }
 
   private[mapcode] def checkNonnull(param: String, obj: AnyRef): Unit =
     require(obj != null, s"Parameter $param should not be null")
